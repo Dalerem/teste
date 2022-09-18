ed95c73deeb2486289969037b759a64bc0e2c589
Dalerem <dalerem2022@gmail.com>
2022-09-18 13:35:19 -0300

    dados inicial

---
+++ testepy.py
@@ -0,0 +1,16 @@
+class main:
+
+    def soma(self):
+        pass
+
+    def sub(self):
+        pass
+
+    def mult(self):
+        pass
+
+    def div(self):
+        pass
+
+    def mostra(self):
+        pass
